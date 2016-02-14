806d11466740c993e4f2fedd630eb83a6253721d
rogozinds <rogozinds@gmail.com>
2016-02-15 00:38:49 +0200

    Add random image generation for heads

--- game.js
+++ game.js
@@ -1,15 +1,14 @@
-var Game = function() {
+var Game = function () {
     // Set the width and height of the scene.
-   // this._width = document.body.clientWidth;
-   // this._height = document.body.clientHeight;
+    // this._width = document.body.clientWidth;
+    // this._height = document.body.clientHeight;
     this._width = 1920;
     this._height = 1440;
-    this._streakLimit=2;
+    this._streakLimit = 2;
     this._center = {
         x: Math.round(this._width / 2),
         y: Math.round(this._height / 2)
     };
-
     // Setup the rendering surface.
     this.renderer = new PIXI.autoDetectRenderer(this._width, this._height);
     document.body.appendChild(this.renderer.view);
@@ -27,11 +26,11 @@ Game.prototype = {
      * Build the scene and begin animating.
      */
 
-    defaultValues: function() {
+    defaultValues: function () {
         this.rocks = [];
         this._streak = 0;
-        this.count=0;
-        this.lives=3;
+        this.count = 0;
+        this.lives = 3;
     },
     build: function () {
         // Draw the background.
@@ -40,7 +39,7 @@ Game.prototype = {
         // Setup the start screen.
         this.setupMenu();
         this.sound = new Howl({
-            src: ['shot.wav'],
+            src: ['shot.wav', 'error.wav'],
             sprite: {
                 shot: [0, 1000]
             }
@@ -111,15 +110,22 @@ Game.prototype = {
             this.startGame();
         }.bind(this);
     },
-
+    nop: function (item) {
+        console.log("Don't shoot at me");
+    },
+    getImagePath: function (isEnemy, nImages) {
+        var rand = Math.floor((Math.random() * nImages) + 1);
+        var folder = isEnemy ? 'enemies' : 'friends';
+        return 'assets/' + folder + '/' + rand + '.png';
+    },
     /**
      * Start the gameplay.
      */
     startGame: function () {
         this.defaultValues();
         // Setup timer to throw random rocks.
-        this.throwHeads();
-
+        this.throwHeads(true, this.onHit, this.headCollapse);
+        this.throwHeads(false, this.onHitWrong, this.nop)
 // create a text object that will be updated...
         this.countingText = new PIXI.Text('SCORE: 0', {
             font: 'bold 50px Arial',
@@ -135,7 +141,7 @@ Game.prototype = {
         this.stage.addChild(this.countingText)
 
         // create a text object that will be updated...
-        this.livesText = new PIXI.Text('Lives: '+this.lives, {
+        this.livesText = new PIXI.Text('Lives: ' + this.lives, {
             font: 'bold 50px Arial',
             fill: '#3e1707',
             align: 'center',
@@ -153,7 +159,7 @@ Game.prototype = {
         this.defaultValues();
         clearTimeout(this.timer);
         // Clear the stage.
-        for (var i=0; i<this.heads.length; i++) {
+        for (var i = 0; i < this.heads.length; i++) {
             if (this.heads[i]) {
                 createjs.Tween.removeTweens(this.heads[i]);
                 this.stage.removeChild(this.heads[i]);
@@ -163,11 +169,24 @@ Game.prototype = {
         this.stage.removeChild(this.livesText);
         this.setupMenu();
     },
-    throwHeads: function () {
+    headCollapse: function (item) {
+        this.stage.removeChild(item);
+        this.lives -= 1;
+        // update the text with a new string
+        this.livesText.text = "LIVES:" + this.lives;
+        this.livesText.updateText();
+        // End game if out of lives.
+        if (this.lives <= 0) {
+            this.endGame();
+        }
+    },
+
+    throwHeads: function (isEnemy, onHit, headCollapse) {
+        var img=this.getImagePath(isEnemy, 3);
         //var rand = Math.ceil(1000 + (Math.random() * 4) * 1000);
-        var rand =2000;
+        var rand = 2000;
         this.timer = setTimeout(function () {
-            var texture = new PIXI.Texture.fromImage('assets/2.png');
+            var texture = new PIXI.Texture.fromImage(img);
 // create a texture from an image path
             var head = new PIXI.Sprite(texture);
 // center the sprites anchor point
@@ -180,26 +199,16 @@ Game.prototype = {
             head.buttonMode = true;
             head.defaultCursor = "crosshair";
             head.alpha = 0.5;
-            head.click = this.onHit.bind(this,head);
-            head.tap = this.onHit.bind(this, head);
+            head.click = onHit.bind(this, head);
+            head.tap = onHit.bind(this, head);
             // Tween the rock with an easing function to simulate physics.
             var y1 = Math.round(50 + Math.random() * 500);
             head._tween1 = createjs.Tween.get(head)
-                .to({alpha:1,y: y1}, 3000,createjs.Ease.cubicOut)
-                .call(function(item) {
-                    this.stage.removeChild(item);
-                    this.lives -= 1;
-                    // update the text with a new string
-                    this.livesText.text = "LIVES:" + this.lives;
-                    this.livesText.updateText();
-                    // End game if out of lives.
-                    if (this.lives <= 0) {
-                        this.endGame();
-                    }
-                }.bind(this,head));
+                .to({alpha: 1, y: y1}, 3000, createjs.Ease.cubicOut)
+                .call(headCollapse.bind(this, head));
             this.heads.push(head);
             this.stage.addChild(head);
-            this.throwHeads();
+            this.throwHeads(isEnemy, onHit, headCollapse);
         }.bind(this), rand);
     },
 
@@ -213,7 +222,7 @@ Game.prototype = {
         requestAnimationFrame(this.tick.bind(this));
     }
     ,
-    animateStreak: function() {
+    animateStreak: function () {
 
         var assetsToLoad = ["assets/gund1.png"];
         // create a new loader
@@ -229,59 +238,59 @@ Game.prototype = {
             var streakPiece = new PIXI.Sprite(texture);
             // this will log the correct width and height as the image was preloaded into the pixi.js cache
             console.log(streakPiece.width + ', ' + streakPiece.height);
-            streakPiece.texture.baseTexture.on('loaded', function(){    console.log(streakPiece.width, streakPiece.height);});
-            streakPiece.anchor.x =0.0;
-            streakPiece.anchor.y =0.0;
-            streakPiece.position.x=this._width-streakPiece.width/2;
-            streakPiece.position.y=-streakPiece.height/2;
-            streakPiece.alpha=0.3;
-            var x=this._width-streakPiece.width;
+            streakPiece.texture.baseTexture.on('loaded', function () {
+                console.log(streakPiece.width, streakPiece.height);
+            });
+            streakPiece.anchor.x = 0.0;
+            streakPiece.anchor.y = 0.0;
+            streakPiece.position.x = this._width - streakPiece.width / 2;
+            streakPiece.position.y = -streakPiece.height / 2;
+            streakPiece.alpha = 0.3;
+            var x = this._width - streakPiece.width;
             var y = 0;
             createjs.Tween.get(streakPiece)
-                .to({x:x,y:y,alpha:0.8}, 1500,createjs.Ease.quadOut)
-                .call(function(streakPiece) {
+                .to({x: x, y: y, alpha: 0.8}, 1500, createjs.Ease.quadOut)
+                .call(function (streakPiece) {
                     this.stage.removeChild(streakPiece);
-                }.bind(this,streakPiece));
+                }.bind(this, streakPiece));
 
-            rampage = new PIXI.Text('RAMPAGE!!!', {
-                font: 'bold 100px Arial',
-                fill: '#ff0000',
-                align: 'center',
-                stroke: 'black',
-                strokeThickness: 2
-            });
-            rampage.anchor.x=0.5;
-            rampage.anchor.y=0.5;
-            rampage.position.x = this._width/2;
-            rampage.position.y= 150;
-            rampage.alpha=0.2;
-            createjs.Tween.get(streakPiece)
-                .to({alpha:1}, 1000,createjs.Ease.quadOut)
-                .to({alpha:1}, 1000,createjs.Ease.quadOut)
-                .call(function(rampage) {
-                    this.stage.removeChild(rampage);
-                }.bind(this,rampage));
-            this.stage.addChild(rampage)
+            this.animateMsg('RAMPAGE!!!');
             this.stage.addChild(streakPiece);
         }
-
-
-
-
-
+    },
+    animateMsg: function (text) {
+        rampage = new PIXI.Text(text, {
+            font: 'bold 100px Arial',
+            fill: '#ff0000',
+            align: 'center',
+            stroke: 'black',
+            strokeThickness: 2
+        });
+        rampage.anchor.x = 0.5;
+        rampage.anchor.y = 0.5;
+        rampage.position.x = this._width / 2;
+        rampage.position.y = 150;
+        rampage.alpha = 0.2;
+        createjs.Tween.get(rampage)
+            .to({alpha: 1}, 2000, createjs.Ease.quadOut)
+            .to({alpha: 0.5}, 1000, createjs.Ease.quadOut)
+            .call(function (rampage) {
+                this.stage.removeChild(rampage);
+            }.bind(this, rampage));
+        this.stage.addChild(rampage)
     },
     onHit: function (item) {
         //explode
         // Create several smaller rocks.
         // Setup the rock sprite.
-        if(this._streakLimit==this._streak) {
-            this._streak=0;
-         this.animateStreak();
+        if (this._streakLimit == this._streak) {
+            this._streak = 0;
+            this.animateStreak();
         }
         this.sound.play('shot');
-        createjs.Tween.get(item).to({alpha:0.3}, 200,createjs.Ease.cubicOut);
-        for (var i=0; i<3;i++) {
-            for(var j=0;j<2;j++) {
+        createjs.Tween.get(item).to({alpha: 0.3}, 200, createjs.Ease.cubicOut);
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 2; j++) {
                 var piece = new PIXI.Sprite.fromImage("assets/blood1.png");
                 piece.width = Math.round(piece.texture.width * 0.43);
                 piece.height = Math.round(piece.texture.height * 0.43);
@@ -291,16 +300,16 @@ Game.prototype = {
                 piece.position.y = item.position.y;
 
                 // Tween the rock.
-                var side=1;
-                if(j!=0) {
-                    side =-1;
+                var side = 1;
+                if (j != 0) {
+                    side = -1;
                 }
                 var x = item.position.x + 30 * side;
                 var y = item.position.y - 30 * i;
                 var t = 800 + Math.round(Math.random() * 100);
 
                 var tween = createjs.Tween.get(piece)
-                    .to({x: x, y: y, rotation: 1, alpha: 0.9}, t, createjs.Ease.quadOut )
+                    .to({x: x, y: y, rotation: 1, alpha: 0.9}, t, createjs.Ease.quadOut)
                     .call(function (obj) {
                         this.stage.removeChild(obj);
                     }.bind(this, piece));
@@ -308,10 +317,9 @@ Game.prototype = {
 
             }
         }
-        this._streak+=1;
-        if(this._streak==this._streakLimit) {
-            this.streak=0;
-            console.log("STREAK!");
+        this._streak += 1;
+        if (this._streak == this._streakLimit) {
+            this.streak = 0;
         }
         //remove
         createjs.Tween.removeTweens(item);
@@ -323,8 +331,11 @@ Game.prototype = {
         this.countingText.text = "SCORE:" + this.count;
         this.countingText.updateText();
     },
-    onHitWrong: function () {
+    onHitWrong: function (item) {
+        createjs.Tween.removeTweens(item);
+        this.stage.removeChild(item);
         this.lives -= 1;
+        this.animateMsg('WRONG TARGET!');
         // update the text with a new string
         this.livesText.text = "LIVES:" + this.lives;
         this.livesText.updateText();
